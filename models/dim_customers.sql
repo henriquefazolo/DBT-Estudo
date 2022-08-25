35032956cd7337f52a23e5c9d9510077093cda5b
Henrique Fazolo <henriquefazolo@gmail.com>
2022-08-25 11:44:53 -0300

    Update dim_customers.sql

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,14 +1,10 @@
-{{ config (
-    materialized="table"
-)}}
-
 with customers as (
 
     select * from {{ ref('stg_customers')}}
 
 ),
 
-with orders as (
+orders as (
 
     select * from {{ ref('stg_orders') }}
 
