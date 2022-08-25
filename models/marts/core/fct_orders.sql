bf70f4f0d47286dca6d7e621990c865df888783f
Henrique Fazolo <henriquefazolo@gmail.com>
2022-08-25 14:31:18 -0300

    Update fct_orders.sql

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,3 +1,24 @@
-with amount as (
+with
+
+amount as (
+
     select * from {{ ref('stg_payments')}}
+),
+
+orders as (
+
+    select * from {{ ref('stg_orders')}}
+),
+
+final as (
+
+    select
+        amount.order_id,
+        orders.customer_id,
+        amount.amount,
+
+    from amount
+    join orders using (order_id)
 )
+
+select * from final
